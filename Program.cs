f412aeae5c27e2740aa0891e1e384c12806a627c
eeooee <epiphany.compton@gmail.com>
2016-11-07 07:59:22 -0600

    more robust program class for testing

--- Program.cs
+++ Program.cs
@@ -8,10 +8,14 @@ namespace CustomList
 {
     class Program
     {
+
         static void Main(string[] args)
         {
 
             CustomList<int> sumList;
+
+            CustomList<int> differenceList;
+            CustomList<int> zipList;
             sumList = new CustomList<int>();
             CustomList<int> myList;
             myList = new CustomList<int>();
@@ -25,25 +29,25 @@ namespace CustomList
             Console.WriteLine("List One \n {0}", message);
             CustomList<int> otherList;
             otherList = new CustomList<int>();
+            otherList.Add(12);
+            otherList.Add(11);
+            otherList.Add(10);
+            otherList.Add(9);
+            otherList.Add(8);
+            otherList.Add(7);
             otherList.Add(6);
-            otherList.Add(5);
-            otherList.Add(4);
-            otherList.Add(3);
-            otherList.Add(2);
-            otherList.Add(1);
-            otherList.Add(0);
             message = otherList.ToString();
             Console.WriteLine("List Two \n {0}", message);
-            sumList = otherList - myList;
-            message = sumList.ToString();
+            differenceList = otherList - myList;
+            message = differenceList.ToString();
             Console.WriteLine("List two minus list one \n {0}", message);
             sumList = myList + otherList;
             message = sumList.ToString();
             Console.WriteLine("List one plus list two \n {0}", message);
-            sumList = otherList.Zip(otherList, myList);
-            message = sumList.ToString();
+            zipList = otherList.Zip(otherList, myList);
+            message = zipList.ToString();
             Console.WriteLine("List one and list two zipped \n {0}", message);
-            sumList.Sort();
+            zipList.Sort();
             message = sumList.ToString();
             Console.WriteLine("Sorted List \n {0}", message);
             Console.ReadLine();
